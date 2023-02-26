dc6ef901c30c84b9bfd002565605891d7af9a001
Diana <d1877087@gmail.com>
2023-02-27 00:56:15 +0200

    Add project files.

---
+++ BooksRating/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BooksRating.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
